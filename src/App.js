3c6428f9dc8895c216f3724340d9b94b75251406
Prasad <sj6282@snaponglobal.com>
2019-12-11 19:21:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// https://developers.google.com/identity/sign-in/web/sign-in - link to get the client ID
+//Implementing Google Sign IN
 import React from 'react';
 import './App.css';
 
@@ -5,7 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1 className="top-heading"> Welcome to the Diurnal</h1>
+        <h2 className="sub-heading">Your daily life  companion</h2>
       </header>
     </div>
   );
